c12b09027962ae4b3d76daae7308c08018d4cb97
LucaTom <luca_sophie@vodafone.de>
2020-04-29 18:42:08 +0200

    xx

--- app/src/main/java/com/example/firsttry/ui/FileReaderAdapter.java
+++ app/src/main/java/com/example/firsttry/ui/FileReaderAdapter.java
@@ -1,11 +1,18 @@
 package com.example.firsttry.ui;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
 
+import com.example.firsttry.R;
 import com.example.firsttry.serialize.FileHelper;
 import com.example.firsttry.serialize.Jsonable;
 import com.example.firsttry.serialize.Todo;
@@ -18,12 +25,47 @@ import java.util.List;
 public class FileReaderAdapter extends ArrayAdapter<Todo> {
 
 
+    private static class ViewHolder {
+        private CheckBox done;
+        private TextView description;
+        private Spinner due;
+    }
+
     private ArrayList<Todo> items;
     private FileHelper<Todo> fileHelper;
 
     public FileReaderAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Todo> todos) {
         super(context, resource, 0, todos);
     }
+
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        ViewHolder viewHolder;
+        if(convertView == null) {
+            convertView = LayoutInflater.from(this.getContext())
+                    .inflate(R.layout.content_lvi_todoliste, parent, false);
+
+            viewHolder = new ViewHolder();
+            viewHolder.done = convertView.findViewById(R.id.chxDone);
+            viewHolder.description = (TextView) convertView.findViewById(R.id.txtDescription);
+            viewHolder.due = convertView.findViewById(R.id.spDue);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        Todo item = getItem(position);
+
+        if(item != null) {
+            viewHolder.done.setChecked(item.done);
+            viewHolder.description.setText(item.description);
+            viewHolder.due.getSelectedItem();
+        }
+
+        return convertView;
+    }
+
 
     public void notifyDataSetChanged() {
         super.notifyDataSetChanged();
